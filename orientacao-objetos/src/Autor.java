9ace577a814fbaf589b1be617dc90e9f1953be48
Suelen Guimarães <suelen.ads.guimaraes@gmail.com>
2019-01-22 12:02:38 -0200

    Adiciona os atributos do autor.

--- orientacao-objetos/src/Autor.java
+++ orientacao-objetos/src/Autor.java
@@ -1,6 +1,17 @@
 
 public class Autor {
-	
+
 	String nome;
 	Editora editora;
+
+	public Autor(String nome, Editora editora) {
+		this.nome = nome;
+		this.editora = editora;
+	}
+
+	@Override
+	public String toString() {
+		return "Autor [nome=" + nome + ", editora=" + editora + "]";
+	}
+
 }

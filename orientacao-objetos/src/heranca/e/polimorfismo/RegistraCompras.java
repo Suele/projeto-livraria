5ec70b7852f76716708b8e6e1a423d4230327b03
Suelen Guimarães <suelen.ads.guimaraes@gmail.com>
2019-01-29 14:14:20 -0200

    Adiciona a classe RegistraCompras para testar o funcionamento dos objetos Autor, Ebook e LivroImpresso.

---
+++ orientacao-objetos/src/heranca/e/polimorfismo/RegistraCompras.java
@@ -0,0 +1,19 @@
+package heranca.e.polimorfismo;
+
+public class RegistraCompras {
+	public static void main(String[] args) throws Throwable {
+
+		Editora editora = new Editora("Casa do Código", "Rua dos Guararapes");
+
+		Autor autor = new Autor("Paulo Silveira", editora);
+
+		Ebook ebook = new Ebook("Orientação de Objetos", 100.00, "123.456.789.00", autor);
+
+		LivroImpresso livroImpresso = new LivroImpresso("Orientação a Objetos", 100, "123.456.789.00", autor);
+
+		CarrinhoDeCompras carrinhoDeCompras = new CarrinhoDeCompras();
+
+		carrinhoDeCompras.adiciona(ebook);
+		carrinhoDeCompras.adiciona(livroImpresso);
+	}
+}

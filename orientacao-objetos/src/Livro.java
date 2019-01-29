80aa1f9e4b58e3e380fa8a032d478d5cdb3cd77a
Suele <suelen.ads.guimaraes@gmail.com>
2019-01-28 23:34:56 -0200

    Adiciona a classe Ebook.

--- orientacao-objetos/src/Livro.java
+++ orientacao-objetos/src/Livro.java
@@ -1,23 +1,21 @@
 
 public class Livro {
 
-	private String nome;
-	private String descricao;
-	private double preco;
-	private String isbn;
-	private int numeroDePaginas;
-	private Autor autor;
+	protected String nome;
+	protected String descricao;
+	protected double preco;
+	protected String isbn;
+	protected int numeroDePaginas;
+	protected Autor autor;
 
-	public Livro(String nome, String descricao, double preco, String isbn, int numeroDePaginas, Autor autor) {
+	public Livro(String nome, double preco, String isbn, Autor autor) {
 		this.nome = nome;
-		this.descricao = descricao;
 		this.preco = preco;
 		this.isbn = isbn;
-		this.numeroDePaginas = numeroDePaginas;
 		this.autor = autor;
 	}
 
-	public double desconto(int valor) {
+	public double aplicaDescontoDe(int valor) {
 		if (valor > 0 && valor < 50) {
 
 			return ((this.preco * valor) / 100);

60fd02759d74ddad8cec3e21538bcc863352bc1c
Suele <suelen.ads.guimaraes@gmail.com>
2019-02-02 20:29:09 -0200

    Como a classe Livro foi alterada para abstrada, as instancias desta classe que estavam na classe Teste foram aleradas para LivroImpresso.

--- orientacao-objetos/src/heranca/e/polimorfismo/Teste.java
+++ orientacao-objetos/src/heranca/e/polimorfismo/Teste.java
@@ -1,15 +1,16 @@
 package heranca.e.polimorfismo;
+
 public class Teste {
 	public static void main(String[] args) throws Throwable {
 
-		Editora editora = new Editora("Casa do CÃ³digo", "Rua dos Guararapes");
+		Editora editora = new Editora("Casa do Código", "Rua dos Guararapes");
 		Autor autor = new Autor("Paulo Silveira", editora);
 
-		Livro livro = new Livro("Estrutura de Dados", 120.90, "123.345.234.90", autor);
+		Livro livroImpresso = new LivroImpresso("Estrutura de Dados", 120.90, "123.345.234.90", autor);
 
-		Ebook ebook = new Ebook("OrientaÃ§Ã£o a objetos", 67.90, "123.453.567.00", autor);
+		Livro ebook = new Ebook("Orientação a objetos", 67.90, "123.453.567.00", autor);
 
-		// System.out.println(livro);
+		// System.out.println(livroImpresso);
 		System.out.println(ebook);
 		System.out.println(ebook.getPreco());
 		System.out.println(ebook.aplicaDescontoDe(0.15));

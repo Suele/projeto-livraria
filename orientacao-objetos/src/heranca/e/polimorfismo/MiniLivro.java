5f13cd10f70ea9716515548f269b5b12a1cfffdd
Suele <suelen.ads.guimaraes@gmail.com>
2019-02-02 20:23:49 -0200

    Adiciona a classe MiniLivro ao projeto.

---
+++ orientacao-objetos/src/heranca/e/polimorfismo/MiniLivro.java
@@ -0,0 +1,11 @@
+package heranca.e.polimorfismo;
+
+public class MiniLivro extends Livro{
+
+	public MiniLivro(String nome, double preco, String isbn, Autor autor) throws Throwable {
+	
+		super(nome, preco, isbn, autor);
+	}
+	
+	
+}

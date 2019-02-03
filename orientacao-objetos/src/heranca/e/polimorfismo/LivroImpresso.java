e0e6cdb5389e022d185dfbd83f715fb0ed95e5d1
Suele <suelen.ads.guimaraes@gmail.com>
2019-02-02 23:15:42 -0200

    Altera o metodo aplicaDescontoDe para valores de desconto de até 30%

--- orientacao-objetos/src/heranca/e/polimorfismo/LivroImpresso.java
+++ orientacao-objetos/src/heranca/e/polimorfismo/LivroImpresso.java
@@ -9,4 +9,17 @@ public class LivroImpresso extends Livro {
 	public double gettaxaDeImpressao() {
 		return super.getPreco() * 0.05;
 	}
+
+	@Override
+	public boolean aplicaDescontoDe(double valorDoDesconto) {
+
+		if (valorDoDesconto > 0 && valorDoDesconto < 0.30) {
+
+			double desconto = getPreco() * valorDoDesconto;
+			setPreco(getPreco() - desconto);
+			return true;
+		} else {
+			return false;
+		}
+	}
 }

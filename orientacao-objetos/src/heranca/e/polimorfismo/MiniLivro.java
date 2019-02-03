53bb7ebbe2f2d24796debbc09849a3cedaa7ba06
Suele <suelen.ads.guimaraes@gmail.com>
2019-02-02 23:13:15 -0200

    Remove o metodo aplicaDescontoDe .

--- orientacao-objetos/src/heranca/e/polimorfismo/MiniLivro.java
+++ orientacao-objetos/src/heranca/e/polimorfismo/MiniLivro.java
@@ -1,11 +1,10 @@
 package heranca.e.polimorfismo;
 
-public class MiniLivro extends Livro{
+public class MiniLivro extends Livro {
 
 	public MiniLivro(String nome, double preco, String isbn, Autor autor) throws Throwable {
-	
+
 		super(nome, preco, isbn, autor);
 	}
-	
-	
+
 }
